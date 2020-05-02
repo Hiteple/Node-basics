134abdeca090ebeb8e802026aca892f5622eb7f0
hiteple <hiteplegold@gmail.com>
2020-05-02 01:32:49 -0300

    Adding nodemon and using modules

---
+++ App.js
@@ -0,0 +1,11 @@
+// Used to require each file
+
+// 1.
+//const singleThreaded = require('./singleThreaded');
+//singleThreaded();
+
+// 2.
+//const name = require('./environment');
+//console.log('Hi ' + name);
+
+// 3.
--- singleThreaded.js
+++ singleThreaded.js
@@ -1,19 +1,18 @@
-console.log('Hello world!');
+function singleThreaded() {
+    console.log('Hello world!');
 
 // If there is an error, the process will TERMINATE. It is SINGLE THREADED
-let i = 0;
-const iteration = setInterval(() => {
-    console.log(i);
-    i++;
+    let i = 0;
+    const iteration = setInterval(() => {
+        console.log(i);
+        i++;
 
-    if (i === 5) {
-        //console.log('We force an error. It WILL STOP');
-        //let a = 3 + z;
-        stopInterval();
-    }
-}, 1000);
-
-function stopInterval() {
-    clearInterval(iteration);
+        if (i === 5) {
+            //console.log('We force an error. It WILL STOP');
+            //let a = 3 + z;
+            clearInterval(iteration);
+        }
+    }, 1000);
 }
 
+module.exports = singleThreaded;
